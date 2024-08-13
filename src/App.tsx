020cf4b10ad5aed1105ab158e782b9830c223a69
Hugo-Farias <hg80rod@gmail.com>
2024-08-13 17:13:08 -0300

    moved navbat into component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
-import logo from "./assets/shared/logo.svg";
-import menu from "./assets/shared/icon-hamburger.svg";
+import NavBar from "./NavBar";
 
 function App() {
   return (
-    <div className={"flex justify-between m-6 h-10 bg-red-950/30"}>
-      <a href={""}>
-        <img className={"h-10 hover:invert"} alt="Logo" src={logo} />
-      </a>
-      <button>
-        <img src={menu} alt="Open Menu" />
-      </button>
+    <div>
+      <NavBar />
     </div>
   );
 }
---
+++ src/NavBar.tsx
@@ -0,0 +1,17 @@
+import logo from "./assets/shared/logo.svg";
+import menu from "./assets/shared/icon-hamburger.svg";
+
+function NavBar() {
+  return (
+    <div className={"flex justify-between m-6 h-10 bg-red-950/30"}>
+      <a href={""}>
+        <img className={"h-10 hover:invert"} alt="Logo" src={logo} />
+      </a>
+      <button>
+        <img src={menu} alt="Open Menu" />
+      </button>
+    </div>
+  );
+}
+
+export default NavBar;
