43bd7d36502cb301a633f3020a10876ed81dccaf
Hugo-Farias <hg80rod@gmail.com>
2024-08-21 16:57:18 -0300

    home redone to be more acurate to original design

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,22 +5,18 @@ const Home = function () {
   setDataSection("home");
 
   return (
-    <div className={"flex flex-col items-center text-center"}>
-      <h3
-        className={
-          "mb-2 mt-11 font-barlowCondensed uppercase tracking-[.168rem]"
-        }
-      >
+    <div className={"mt-11 flex flex-col items-center text-center"}>
+      <h3 className={"mb-2 font-barlowCondensed uppercase tracking-[.17rem]"}>
         So, you want to travel to
       </h3>
       <h1
         className={
-          "font-bellefair text-[4.7rem] uppercase tracking-wider text-white"
+          "mb-3 font-bellefair text-[4.7rem] uppercase tracking-wider text-white"
         }
       >
         Space
       </h1>
-      <p className={"text-[0.95rem] font-light"}>
+      <p className={"text-[0.95rem] font-light leading-[1.56rem]"}>
         Let’s face it; if you want to go to space, you might as well genuinely
         go to outer space and not hover kind of on the edge of it. Well sit
         back, and relax because we’ll give you a truly out of this world
@@ -29,7 +25,7 @@ const Home = function () {
 
       <Link
         className={
-          "mt-5 flex size-[150px] items-center justify-center rounded-[50%] bg-white font-bellefair uppercase text-black"
+          "mt-20 flex size-[150px] items-center justify-center rounded-[50%] bg-white font-bellefair text-[1.4rem] uppercase text-black"
         }
         to={"/destination"}
       >
