f4632bc796ead466689ce098a5530282d28dd387
Hugo-Farias <hg80rod@gmail.com>
2024-09-07 23:47:41 -0300

    add: section change start

---
+++ src/components/MenuButton.tsx
@@ -0,0 +1,21 @@
+type PropT = {
+  open: boolean;
+};
+
+const MenuButton = function ({ open }: PropT) {
+  return (
+    <div className={"space-y-1.5 opacity-80"}>
+      <div
+        className={`h-0.5 w-6 rounded-md bg-white transition-transform duration-500 ${open && "translate-y-2 rotate-45"}`}
+      />
+      <div
+        className={`h-0.5 w-6 rounded-md bg-white transition-opacity duration-1000 ${open && "opacity-0"}`}
+      />
+      <div
+        className={`h-0.5 w-6 rounded-md bg-white transition-transform duration-500 ${open && "-translate-y-2 -rotate-45"}`}
+      />
+    </div>
+  );
+};
+
+export default MenuButton;
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,17 +1,20 @@
 import logo from "../assets/shared/logo.svg";
 import Link from "../router/Link.tsx";
 import { useState } from "react";
-import Menu from "./Menu.tsx";
+import MenuButton from "./MenuButton.tsx";
+import { AnimatePresence, motion } from "framer-motion";
 
 function NavBar() {
   const [isOpen, setIsOpen] = useState<boolean>(false);
 
   function toggleMenu() {
+    document.body.style.overflow = isOpen ? "auto" : "hidden";
+
     setIsOpen(!isOpen);
   }
 
   return (
-    <div className={"flex h-10 items-center justify-between"}>
+    <nav className={"z-10 flex h-10 items-center justify-between"}>
       <Link className={"rounded-[50%]"} to={"/"}>
         <img
           className={"h-10 rounded-[50%] border border-stone-500 hover:invert"}
@@ -19,10 +22,30 @@ function NavBar() {
           src={logo}
         />
       </Link>
-      <button className={"flex gap-6"} onMouseDown={toggleMenu}>
-        <Menu open={isOpen} />
+      <button className={"z-10 flex gap-6"} onMouseDown={toggleMenu}>
+        <MenuButton open={isOpen} />
       </button>
-    </div>
+      <AnimatePresence>
+        {isOpen && (
+          <>
+            <motion.div
+              className={
+                "fixed right-0 top-0 h-screen w-64 rounded-bl-md rounded-tl-md border border-white/15 bg-gray-500/5 backdrop-blur-xl backdrop-saturate-100"
+              }
+              initial={{ opacity: 1, x: "100%" }}
+              animate={{ opacity: 1, x: 0 }}
+              exit={{ opacity: 1, x: "100%" }}
+              transition={{ duration: 0.2 }}
+              key={"menu"}
+            />
+            <div
+              onClick={toggleMenu}
+              className={"fixed left-0 top-0 h-screen w-screen"}
+            />
+          </>
+        )}
+      </AnimatePresence>
+    </nav>
   );
 }
 
