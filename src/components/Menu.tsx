62c954516e27075d14a08a7166aa11eea1c91577
Hugo-Farias <hg80rod@gmail.com>
2024-09-07 21:46:51 -0300

    rollback

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -6,11 +6,13 @@ const Menu = function ({ open }: PropT) {
   return (
     <div className={"space-y-1.5 overflow-hidden"}>
       <div
-        className={`h-0.5 w-6 bg-white ${open && "translate-y-2 -rotate-45"}`}
+        className={`h-0.5 w-6 bg-white transition-transform duration-700 ${open && "translate-y-2 -rotate-45"}`}
       />
-      <div className={`h-0.5 w-6 bg-white ${open && "opacity-0"}`} />
       <div
-        className={`h-0.5 w-6 bg-white ${open && "-translate-y-2 rotate-45"}`}
+        className={`h-0.5 w-6 bg-white transition-opacity duration-500 ${open && "opacity-0"}`}
+      />
+      <div
+        className={`h-0.5 w-6 bg-white transition-transform duration-700 ${open && "-translate-y-2 rotate-45"}`}
       />
     </div>
   );
